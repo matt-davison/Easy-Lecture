f0eab4b764076f619c96e0a264cd2a133d202f50
BlazeFace <nateb@vt.edu>
2020-02-15 14:22:57 -0500

    Update fs.py
    Added get course names for user

--- fs.py
+++ fs.py
@@ -1,8 +1,11 @@
 import os
 
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'C:\\vthacks7.json'
+
 from google.cloud import firestore
 
 db = firestore.Client()
+
 
 # for doc in users_ref.stream():
 #    print(u'{} => {}'.format(doc.id, doc.to_dict()))
@@ -14,15 +17,15 @@ def get_lecture_by_name(dept, class_name, lecture_name):
         .document(class_name).collection(u'Lectures').document(lecture_name)
     return ref.get().to_dict()
 
+
 def get_courses_by_department(dept):
     ref = db.collection(u'department').document(dept).get().to_dict()
     return ref
 
-#doc = get_lecture_by_name('CS', '2505', 'Intro to C')
-#print(doc)
+def get_course_names_for_user(user):
+    ref = db.collection(u'users').document(user).get().to_dict()
+    courses = []
+    for r in ref['courses']:
+        courses.append(r.get().to_dict()['name'])
 
-#doc2 = get_courses_by_department('CS')
-'''
-for d in doc2:
-    print(d)
-'''
+    return courses
