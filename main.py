25521df451ed44110a6103c60882fa8ef7accb81
Matt Davison <mattd7@vt.edu>
2020-02-14 23:29:18 -0500

    adding more authentication stuff

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 import datetime
-
 from flask import Flask, render_template, jsonify, request
 from google.auth.transport import requests
 from google.cloud import datastore
 import google.oauth2.id_token
 
+datastore_client = datastore.Client()
+
 app = Flask(__name__)
 
 firebase_request_adapter = requests.Request()
+
+def store_time(dt):
+    entity = datastore.Entity(key=datastore_client.key('visit'))
+    entity.update({
+        'timestamp': dt
+    })
+
+    datastore_client.put(entity)
+
+def fetch_times(limit):
+    query = datastore_client.query(kind='visit')
+    query.order = ['-timestamp']
+
+    times = query.fetch(limit=limit)
+
+    return times
+
 @app.route('/')
 def root():
     # Verify Firebase auth.
@@ -55,5 +73,3 @@ def login():
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
-
-# [START gae_python37_render_template]
