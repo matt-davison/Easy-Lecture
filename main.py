9768df74ab36215baa5579de28ad709a6b0327ee
Matt Davison <mattd7@vt.edu>
2020-02-15 21:19:42 -0500

    added gunicorn

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import datetime
 from flask import Flask, render_template, jsonify, request, make_response, session, redirect, url_for
 import os
 from werkzeug.utils import secure_filename
-#import logging
 from db_upload import upload_blob
 from firestore_manager import get_course_names_for_user
 
@@ -82,8 +81,6 @@ def upload_lecture():
 			f.seek(int(request.form['dzchunkbyteoffset']))
 			f.write(file.stream.read())
 	except OSError:
-		# log.exception will include the traceback so we can see what's wrong 
-		#log.exception('Could not write to file')
 		return make_response(("Not sure why,"
 							  " but we couldn't write the file to disk", 500))
 
@@ -92,17 +89,11 @@ def upload_lecture():
 	if current_chunk + 1 == total_chunks:
 		# This was the last chunk, the file should be complete and the size we expect
 		if os.path.getsize(save_path) != int(request.form['dztotalfilesize']):
-			'''log.error(f"File {file.filename} was completed, "
-					  f"but has a size mismatch."
-					  f"Was {os.path.getsize(save_path)} but we"
-					  f" expected {request.form['dztotalfilesize']} ")'''
+			print("expected file size of {request.form['dztotalfilesize']} but was {os.path.getsize(save_path)}")
 			return make_response(('Size mismatch', 500))
 		else:
 			#log.info(f'File {file.filename} has been uploaded successfully')
 			upload_blob(temp_dir, secure_name)
-	else:
-		'''log.debug(f'Chunk {current_chunk + 1} of {total_chunks} '
-				  f'for file {file.filename} complete')'''
 
 	return make_response(("Chunk upload successful", 200))
 
